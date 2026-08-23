25b3b9b009da3362b635cde25d951ef4cfee081b
Ritik kharya <158287555+RITIK-KHARYA@users.noreply.github.com>
2026-08-23 16:11:21 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+   public:
+    bool isArraySpecial(vector<int>& nums) {
+        int length = nums.size();
+        if (length == 1) return true;
+        for (int i = 0; i < length-1; i++) {
+             if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0 ||
+                     nums[i] % 2 != 0 && nums[i + 1] % 2 != 0)
+                return false;
+        }
+        return true;
+    }
+};
